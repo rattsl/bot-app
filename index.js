d0614aaae5e5483e1bb509b2d69dedc262390457
Hayato Kakiuchi <qweaty.uiop8810@i.softbank.jp>
2021-02-01 03:53:57 +0900

    Improve

--- index.js
+++ index.js
@@ -26,60 +26,28 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "江端"){
+            if (event.message.text == "かっこいい"){
                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
-                    text: "はよ出てこい"
+                    text: "ありがと❤️"
                 }));
-            }　else if (event.message.text == "わかまる"){
+            }　else if (event.message.text == "すごい"){
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
-                    text: "はよ出てこい"
+                    text: "どういたしまして"
                 }));
-            }　else if (event.message.text == "ゆうすけ"){
+            }　else if (event.message.text == "イケメン"){
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
-                    text: "新幹線代ちょうだい"
+                    text: "よく言われます"
                 }));
             }　else if (event.message.text == "かきうちくん"){
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
                     text: "尊敬するわ〜"
                 }));
-            } else if (event.message.text == "目の前に爆弾がある！"){
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "5秒後に爆発します。"
-                }));
-            } else if (event.message.text == "ただいま"){
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "おかえり"
-                }));
-            }　else if (event.message.text == "メガネの種類は？"){
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: ""
-                }));
-            }　else if (event.message.text == ""){
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "絶対にいや"
-                }));
-            }　else if (event.message.text == ""){
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "死んでもいや"
-                }));
-            } else if (event.message.text == "ともき"){
-                events_processed.push(bot.replyMessage(event.replyToken, {
-                    type: "text",
-                    text: "いけめん"
-                }));
-            }
-
+            } 
 
         }
     });
